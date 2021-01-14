ca8beecdf88fd8f217d8de04e061cc0cc2efd600
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-01-14 00:44:32 -0800

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBsnTl5saWWyKN6cyAKr-rI6_JlkZsaBkc",
+    authDomain: "oshop-eb273.firebaseapp.com",
+    projectId: "oshop-eb273",
+    storageBucket: "oshop-eb273.appspot.com",
+    messagingSenderId: "445868425919",
+    appId: "1:445868425919:web:c51ff936089193d1b4fc0a",
+    measurementId: "G-9G0X9JBDP6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBsnTl5saWWyKN6cyAKr-rI6_JlkZsaBkc",
+    authDomain: "oshop-eb273.firebaseapp.com",
+    projectId: "oshop-eb273",
+    storageBucket: "oshop-eb273.appspot.com",
+    messagingSenderId: "445868425919",
+    appId: "1:445868425919:web:c51ff936089193d1b4fc0a",
+    measurementId: "G-9G0X9JBDP6"
+  }
 };
 
 /*
